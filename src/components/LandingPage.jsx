8cff93dd3f6b30ff04138eea6c28f1945b6effdb
Aditya201514049 <adityasingha084@gmail.com>
2025-07-08 01:15:43 +0600

    refactor: improve code readability in LandingPage component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,18 +5,22 @@ function LandingPage() {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
       <div className='textstructure mt-40 px-10'>
-        {["We create", "Eye - Opening", "Presentations"].map((item, index)=>{
+        {["We create", "Eye - Opening", "Presentations"].map((item, index) => {
           return <div className='masker'>
-          <h1 className="uppercase text-7xl leading-none tracking-tighter font-semibold font-['Test_Founders_Grotesk_X-Condensed']">
-            {item}
-          </h1>
-        </div>
+            <div className='w-fit flex items-center'>
+              {index === 1 && (<div className='w-[9vw] h-[5vw] bg-red-500'></div>)}
+              <h1 className="uppercase text-7xl leading-none tracking-tighter font-semibold font-['Test_Founders_Grotesk_X-Condensed']">
+                {item}
+              </h1>
+            </div>
+
+          </div>
         })}
-        
+
       </div>
       <div className='border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20'>
         {[
-          "For public and private companies", 
+          "For public and private companies",
           "From the first pitch to IPO"
         ].map((item, index) => (
           <p key={index} className='text-md font-light tracking-tight leading-none'>{item}</p>
